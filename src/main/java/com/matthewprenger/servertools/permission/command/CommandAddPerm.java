3b17f3ee2852895fc60614f70f36ad8efb8da0d6
Matthew Prenger <matthewprengermc@gmail.com>
2014-06-28 12:34:10 -0500

    Forgot some stuff

---
+++ src/main/java/com/matthewprenger/servertools/permission/command/CommandAddPerm.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright 2014 Matthew Prenger
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.matthewprenger.servertools.permission.command;
+
+import com.matthewprenger.servertools.core.command.CommandLevel;
+import com.matthewprenger.servertools.core.command.ServerToolsCommand;
+import com.matthewprenger.servertools.permission.Group;
+import com.matthewprenger.servertools.permission.perms.PermissionManager;
+import net.minecraft.command.CommandHandler;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.command.PlayerNotFoundException;
+import net.minecraft.command.WrongUsageException;
+import net.minecraft.server.MinecraftServer;
+
+import java.util.Collection;
+import java.util.List;
+
+public class CommandAddPerm extends ServerToolsCommand {
+
+    public CommandAddPerm(String defaultName) {
+        super(defaultName);
+    }
+
+    @Override
+    public CommandLevel getCommandLevel() {
+        return CommandLevel.OP;
+    }
+
+    @Override
+    public List addTabCompletionOptions(ICommandSender sender, String[] strings) {
+
+        if (strings.length == 1) {
+            CommandHandler ch = (CommandHandler) MinecraftServer.getServer().getCommandManager();
+            return ch.getPossibleCommands(sender, strings[0]);
+        } else if (strings.length == 2) {
+            Collection<String> groupKeys = PermissionManager.getGroupNames();
+            return getListOfStringsMatchingLastWord(strings, groupKeys.toArray(new String[groupKeys.size()]));
+        } else
+            return null;
+
+    }
+
+    @Override
+    public String getCommandUsage(ICommandSender sender) {
+
+        return "/" + name + " [perm] [groupname]";
+    }
+
+    @Override
+    public void processCommand(ICommandSender sender, String[] args) {
+
+        if (args.length != 2)
+            throw new WrongUsageException(getCommandUsage(sender));
+
+        Group group = PermissionManager.getGroup(args[1]);
+
+        if (group == null)
+            throw new PlayerNotFoundException("That group doesn't exist");
+
+        group.addPerm(args[0]);
+
+        func_152373_a(sender, this, String.format("Added perm %s to %s", args[0], args[1]));
+    }
+}
